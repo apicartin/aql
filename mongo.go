2c7faa15b5f5b3db9aa595092caa211645b8cad4
Vinay Kumar <bit.vinay1@gmail.com>
2021-06-03 17:56:48 +0530

    feat: sql support

---
+++ mongo.go
@@ -0,0 +1,79 @@
+package aql
+
+import (
+	"encoding/json"
+	"strconv"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var d JSONDecoder
+
+type MongoParser struct {
+}
+
+func (mp MongoParser) Parse(f string) interface{} {
+	initMap()
+	sqlMap := bson.M{}
+	andAr := []bson.M{}
+	r := d.Decode(f)
+
+	for k, v := range r {
+
+		cri1 := v[0]
+		cond1 := bson.M{mongoOpMap[cri1.Operator]: handleInt64(cri1.Value)}
+		if len(v) == 2 {
+			// and operator
+			cri2 := v[1]
+
+			cond2 := bson.M{mongoOpMap[cri2.Operator]: handleInt64(cri2.Value)}
+			andAr = append(andAr, bson.M{k: cond1})
+			andAr = append(andAr, bson.M{k: cond2})
+		} else if len(v) == 1 {
+			andAr = append(andAr, bson.M{k: cond1})
+		}
+	}
+	sqlMap["$and"] = andAr
+
+	return sqlMap
+}
+
+func (mp MongoParser) Sort(f string) interface{} {
+	result := bson.M{}
+	r := make(map[string]string)
+	err := json.Unmarshal([]byte(f), &r)
+	if err != nil {
+		return result
+	}
+	for k, v := range r {
+		if v == "asc" {
+			result[k] = 1
+		} else if v == "desc" {
+			result[k] = -1
+		}
+	}
+	return result
+}
+
+func handleInt64(v interface{}) interface{} {
+
+	switch v.(type) {
+	case string:
+
+		n, err := strconv.ParseInt(v.(string), 10, 64)
+		if err == nil {
+
+			if len(v.(string)) == 19 {
+				return n
+			}
+		}
+		break
+	case []interface{}:
+		newArr := []interface{}{}
+		for _, vv := range v.([]interface{}) {
+			newArr = append(newArr, handleInt64(vv))
+		}
+		v = newArr
+	}
+	return v
+}
---
+++ sql.go
@@ -0,0 +1,86 @@
+package aql
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+type SqlParser struct {
+}
+
+func (mp SqlParser) Parse(f string) interface{} {
+	initSqlMap()
+	sql := ""
+	conditions := []string{}
+	r := d.Decode(f)
+
+	for k, v := range r {
+
+		cri1 := v[0]
+		cond1 := fmt.Sprintf(sqlOpMap[cri1.Operator], k, handleInt64ForSql(cri1.Value))
+		if len(v) == 2 {
+			// and operator
+			cri2 := v[1]
+
+			cond2 := fmt.Sprintf(sqlOpMap[cri2.Operator], k, handleInt64ForSql(cri2.Value))
+			conditions = append(conditions, cond1+" and "+cond2)
+		} else if len(v) == 1 {
+
+			conditions = append(conditions, cond1)
+		}
+	}
+
+	for i, s := range conditions {
+		if i == len(conditions)-1 {
+			sql = sql + fmt.Sprintf(" ( %s ) ", s)
+		} else {
+			sql = sql + fmt.Sprintf(" ( %s ) and ", s)
+		}
+	}
+	return sql
+}
+
+func (mp SqlParser) Sort(f string) interface{} {
+	sql := "order by "
+	r := make(map[string]string)
+	err := json.Unmarshal([]byte(f), &r)
+	if err != nil {
+		return ""
+	}
+
+	for k, v := range r {
+		sql = sql + " " + k + " " + v
+	}
+	return sql
+}
+
+func handleInt64ForSql(v interface{}) interface{} {
+
+	switch v.(type) {
+	case string:
+
+		n, err := strconv.ParseInt(v.(string), 10, 64)
+		if err == nil {
+
+			if len(v.(string)) == 19 {
+				return n
+			}
+		}
+		break
+
+	case []interface{}:
+		val := ""
+		for i, vv := range v.([]interface{}) {
+			str1 := fmt.Sprintf("%v", handleInt64ForSql(vv))
+			if i == len(v.([]interface{}))-1 {
+				val = val + str1
+			} else {
+				val = val + str1 + " , "
+			}
+
+		}
+		v = val
+	}
+	return v
+}
